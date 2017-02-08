b2e439ae6e63ac4f9127ea71150505c02ab107db
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2017-02-08 11:31:28 +0000

    Update and improve setup.py file

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -1,33 +1,17 @@
 from setuptools import setup
 
-# Importing the "multiprocessing" module is required for the "nose.collector".
-# See also: http://bugs.python.org/issue15881#msg170215
-try:
-    import multiprocessing
-except ImportError:
-    pass
-
-# Define the test runner.
-# See also:
-# http://fgimian.github.io/blog/2014/04/27/running-nose-tests-with-plugins-using-the-python-setuptools-test-command/
-from setuptools.command.test import test as TestCommand
-class NoseTestCommand(TestCommand):
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-    def run_tests(self):
-        # Run nose ensuring that argv simulates running nosetests directly.
-        import nose
-        nose.run_exit(argv=['nosetests'])
-
+url = ""
 version = "{{ cookiecutter.version }}"
 readme = open('README.rst').read()
 
 setup(name="{{ cookiecutter.repo_name }}",
-      version=version,
-      long_description=readme,
       packages=["{{ cookiecutter.repo_name}}"],
-      cmdclass={"test": NoseTestCommand},
-      tests_require=["nose", "coverage"],
-)
+      version=version,
+      description="{{ cookiecutter.description }}",
+      long_description=readme,
+      include_package_data=True,
+      author="{{ cookiecutter.author }}",
+      author_email="{{ cookiecutter.author_email }}",
+      url=url,
+      download_url="{}/tarball/{}".format(url, version),
+      license="MIT")
