58c90986572b54063edc5809700140bd650f8bf4
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2017-08-01 09:52:21 +0100

    Improve formatting of setup.py

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -4,15 +4,17 @@ url = ""
 version = "{{ cookiecutter.version }}"
 readme = open('README.rst').read()
 
-setup(name="{{ cookiecutter.repo_name }}",
-      packages=["{{ cookiecutter.repo_name}}"],
-      version=version,
-      description="{{ cookiecutter.description }}",
-      long_description=readme,
-      include_package_data=True,
-      author="{{ cookiecutter.author }}",
-      author_email="{{ cookiecutter.author_email }}",
-      url=url,
-      install_requires=[],
-      download_url="{}/tarball/{}".format(url, version),
-      license="MIT")
+setup(
+    name="{{ cookiecutter.repo_name }}",
+    packages=["{{ cookiecutter.repo_name}}"],
+    version=version,
+    description="{{ cookiecutter.description }}",
+    long_description=readme,
+    include_package_data=True,
+    author="{{ cookiecutter.author }}",
+    author_email="{{ cookiecutter.author_email }}",
+    url=url,
+    install_requires=[],
+    download_url="{}/tarball/{}".format(url, version),
+    license="MIT"
+)
